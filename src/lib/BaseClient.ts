5ae183f224d002613bbdd7349741c3acab9cb685
Rygent <46462403+Rygent@users.noreply.github.com>
2023-04-07 11:08:29 +0700

    chore: cleanup

--- src/lib/BaseClient.ts
+++ src/lib/BaseClient.ts
@@ -44,13 +44,14 @@ export default class BaseClient<Ready extends boolean = boolean> extends Client<
 		this.events = new Collection();
 
 		this.utils = new Util(this);
+
+		this.version = options.version;
+		this.debug = options.debug;
 	}
 
 	private validate(options: ClientOptions) {
 		if (typeof options !== 'object') throw new TypeError('Options should be a type of Object.');
 		if (semver.lt(process.versions.node, '16.14.0')) throw new Error('This client requires Node.JS v16.14.0 or higher.');
-		this.version = options.version;
-		this.debug = options.debug;
 
 		if (!options.token) throw new Error('You must pass the token for the Client.');
 		this.token = options.token;
